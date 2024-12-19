3eb70235097bf29dd37292aca3d2d18fdc10e8be
JulesBestDev176 <souleymanefall176@gmail.com>
2024-12-19 17:19:29 +0000

    first commit

---
+++ ProjetConsole1/Exo3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string saisie;
+            bool verif = false;
+            string rep;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Saisir le nombre : ");
+                    saisie = Console.ReadLine();
+
+                    if(int.TryParse(saisie, out n))
+                    {
+                        verif = true;
+                        MultiplicationFor(n);
+                    }else
+                    {
+                        Console.WriteLine("Veuillez saisir un nombre valide");
+                    }
+                } while (!verif);
+
+                Console.WriteLine("Pour quitter, saisir Q");
+                rep = Console.ReadLine();
+                if(rep.ToUpper() == "Q")
+                {
+                    Console.WriteLine("Fin du programme");
+                }
+            } while (rep.ToUpper() != "Q");
+
+            
+        }
+
+        /// <summary>
+        /// Cette fonction permet d'aficher la table de multiplication en utilisant la boucle for
+        /// </summary>
+        /// <param name="n">Saisir la table</param>
+
+        public static void MultiplicationFor(int n)
+        {
+            for (int i = 0; i <= 12; i++)
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+            }
+        }
+
+        /// <summary>
+        /// Cette fonction permet d'aficher la table de multiplication en utilisant la boucle while
+        /// </summary>
+        /// <param name="n"></param>
+        public static void MultiplicationWhile(int n)
+        {
+            int i = 0;
+            while(i <= 12)
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+                i++;
+            }
+        }
+        /// <summary>
+        /// Cette fonction permet d'aficher la table de multiplication en utilisant la boucle do while
+        /// </summary>
+        /// <param name="n"></param>
+        public static void MultiplicationDoWhile(int n)
+        {
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+                i++;
+            } while (i <= 12);
+        }
+    }
+}
---
+++ ProjetConsole1/Exo4/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+        string saisie;
+        bool verif = false;
+
+        do
+        {
+            Console.WriteLine("Saisir un entier positif : ");
+            saisie = Console.ReadLine();
+
+            if(int.TryParse(saisie, out n))
+            {
+                if(n < 0)
+                {
+                    Console.WriteLine("Vous devez saisir un entier positif.");
+                }
+                else
+                {
+                    Console.WriteLine($"Le factoriel de {n} est {Factoriel(n)}.");
+                    verif = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Veuillez saisir un nombre valide");
+            }
+        } while (!verif);
+    }
+
+    /// <summary>
+    /// Cette fonction permet de calculer le factoriel d'un nombre saisie
+    /// </summary>
+    /// <param name="n">Saisir un entier positif</param>
+    /// <returns>Retourner le factoriel de cet entier positif </returns>
+    public static int Factoriel(int n)
+    {
+        int fact = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            fact *= i;
+        }
+        return fact;
+    }
+}
